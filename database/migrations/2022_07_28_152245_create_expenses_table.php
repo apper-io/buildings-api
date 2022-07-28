c50e77ed75ae08bdc6685ca8ff774e90da22c196
Adrian <adrianmarrero18@gmail.com>
2022-07-28 11:29:43 -0400

    ExpenseModel --all created

--- database/migrations/2022_07_28_152245_create_expenses_table.php
+++ database/migrations/2022_07_28_152245_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBillsTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateBillsTable extends Migration
      */
     public function up()
     {
-        Schema::create('bills', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->string('bill_code');
-            $table->string('month_year_code');
-            $table->float('amount', 8, 2);
+            $table->string('name');
+            $table->text('description')->nullable();
             $table->foreignId('status_id');
             $table->foreignId('type_id');
             $table->timestamps();
@@ -31,6 +30,6 @@ class CreateBillsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bills');
+        Schema::dropIfExists('expenses');
     }
 }
